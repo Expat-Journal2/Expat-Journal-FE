570866d994a23e1455f9583d887a7a58b5298c82
tippitytapp <marc.tapp@gmail.com>
2020-04-28 10:18:27 -0400

    updated some routes, added home component

--- expat-journal-fe/src/App.js
+++ expat-journal-fe/src/App.js
@@ -4,6 +4,8 @@ import LoginForm from "./components/LoginForm"
 import FormValidation from './utils/validation'
 import {Route, Switch, Link} from "react-router-dom"
 import PrivateRoute from "./components/PrivateRoute"
+import LoggedOut from "./components/LoggedOut"
+import Home from "./components/Home"
 
 import './App.css';
 import Dashboard from './components/Dashboard';
@@ -17,15 +19,14 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Foreigner Files</h1>
-      <h2>The place for you to show off where you've been and what you've done!</h2>
-      <Link to="/register"><button>Sign Up</button></Link>
-      <Link to="/login"><button>Log In</button></Link>
+
 
       <Switch>
+      <Route exact path="/"><Home/></Route>
       <Route exact path="/register"><SignUpForm /></Route>
       <Route exact path="/login"><LoginForm /></Route>
       <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      <Route path="/loggedout"><LoggedOut /></Route>
       </Switch>
     </div>
   );
