f6af5cb1be35d063a7572da87723a0477ea1ae01
tippitytapp <marc.tapp@gmail.com>
2020-04-27 20:34:04 -0400

    added links to the main component for login and signup

--- expat-journal-fe/src/App.js
+++ expat-journal-fe/src/App.js
@@ -19,8 +19,11 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Expat-Journal-2</h1>
-      <h2>This is a temporary Home Page Component</h2>
+      <h1>Foreigner Files</h1>
+      <h2>The place for you to show off where you've been and what you've done!</h2>
+      <Link to="/register"><button>Sign Up</button></Link>
+      <Link to="/login"><button>Log In</button></Link>
+
       <Switch>
       <Route exact path="/register"><SignUpForm /></Route>
       <Route exact path="/login"><LoginForm /></Route>
