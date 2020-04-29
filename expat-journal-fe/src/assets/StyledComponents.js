1687f095f11241312ea2e247f077401506a2c37a
Guillermo Alfaro <galfarotolon@gmail.com>
2020-04-29 16:26:17 -0500

    work in progress

---
+++ expat-journal-fe/src/assets/StyledComponents.js
@@ -0,0 +1,31 @@
+// dependency imports
+import styled from "styled-components";
+
+
+/////HeaderBar styled components 
+
+export const Navbar = styled.div`
+width: 100%;
+background: rgba(0, 0, 0, 0.4);
+display: flex;
+flex-direction: row;
+margin: 0 auto;
+justify-content: space-around;
+padding: 1% 0;
+`
+export const OtherLinks = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+width: 70%;
+`
+
+export const MoreLinks = styled.div`
+display: flex;
+flex-direction: row;
+width: 10%;
+justify-content: space-around;
+`
+export const Span = styled.span`
+color: orange;
+`
--- expat-journal-fe/src/components/HeaderBar.js
+++ expat-journal-fe/src/components/HeaderBar.js
@@ -4,47 +4,23 @@ import { Link, useHistory } from "react-router-dom"
 import { fetchAllPosts } from "../store/actions"
 import styled from 'styled-components';
 import AddPost from "./AddPost"
-import {Button} from "reactstrap"
-
-const Navbar = styled.div`
-width: 100%;
-background: rgba(0, 0, 0, 0.4);
-display: flex;
-flex-direction: row;
-margin: 0 auto;
-justify-content: space-around;
-padding: 1% 0;
-`
-const OtherLinks = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-width: 70%;
-`
-const MoreLinks = styled.div`
-display: flex;
-flex-direction: row;
-width: 10%;
-justify-content: space-around;
-`
-const Span = styled.span`
-color: orange;
-`
-
+import { Button } from "reactstrap"
+//styled components
+import { Navbar, OtherLinks, MoreLinks, Span } from "../assets/StyledComponents"
 
 
 function HeaderBar(props) {
 
-    const {push} = useHistory();
+    const { push } = useHistory();
     const dispatch = useDispatch();
 
     const [modal, setModal] = useState(false)
     const toggle = () => setModal(!modal)
-        const LogOut = event => {
+    const LogOut = event => {
         event.preventDefault();
         localStorage.removeItem('token');
         localStorage.removeItem('userId')
-        dispatch({type: "LOG_OUT"})
+        dispatch({ type: "LOG_OUT" })
         push("/loggedout")
     }
 
@@ -58,16 +34,16 @@ function HeaderBar(props) {
                 <Link to="/allposts" className="ATag">View All Posts</Link>
                 <Link to="#" className="ATag">About</Link>
             </OtherLinks>
-            {!props.isLoggedIn && (            <MoreLinks>
+            {!props.isLoggedIn && (<MoreLinks>
                 <Link to="/register" className="ATag">Signup <Span> | </Span></Link>
-                <Link to="/login"  className="ATag">Login</Link>
+                <Link to="/login" className="ATag">Login</Link>
             </MoreLinks>)}
             {props.isLoggedIn && (<MoreLinks>
-                <Link onClick={toggle}  className="ATag"><i class="fas fa-plus"></i></Link>
-                <AddPost show={modal} toggle={toggle}/>
-                <Link onClick={LogOut}  className="ATag" alt="sign out"><i class="fas fa-sign-out-alt" alt="sign out"></i></Link>
+                <Link onClick={toggle} className="ATag"><i class="fas fa-plus"></i></Link>
+                <AddPost show={modal} toggle={toggle} />
+                <Link onClick={LogOut} className="ATag" alt="sign out"><i class="fas fa-sign-out-alt" alt="sign out"></i></Link>
 
-                </MoreLinks>)}
+            </MoreLinks>)}
 
         </Navbar>
     )
