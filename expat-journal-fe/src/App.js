33e92072465911f08eac8a79a6e7522a462bb8ff
tippitytapp <marc.tapp@gmail.com>
2020-04-29 17:04:35 -0400

    pushing up

--- expat-journal-fe/src/App.js
+++ expat-journal-fe/src/App.js
@@ -1,43 +1,38 @@
-import React from 'react';
+// dependency imports
+import React from 'react'
+import { Route, Switch, Link } from "react-router-dom"
+// util imports
+import FormValidation from './utils/validation'
+
+// component imports
 import SignUpForm from "./components/SignUpForm"
 import LoginForm from "./components/LoginForm"
-import FormValidation from './utils/validation'
-import { Route, Switch, Link } from "react-router-dom"
 import PrivateRoute from "./components/PrivateRoute"
 import LoggedOut from "./components/LoggedOut"
 import Home from "./components/Home"
-import './App.css';
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard'
 import HeaderBar from "./components/HeaderBar"
-import AllPosts from './components/AllPosts';
-import AddPost from './components/AddPost';
+import AllPosts from './components/AllPosts'
+import AddPost from './components/AddPost'
 
-
-
-
-
-
-
+// styling imports
+import './App.css';
 
 function App() {
-
-  return (
-    <div className="App">
-      <HeaderBar />
-
-      <Switch>
-        <Route exact path="/"><Home /></Route>
-        <Route exact path="/register"><SignUpForm /></Route>
-        <Route exact path="/login"><LoginForm /></Route>
-        <Route exact path="/allposts"><AllPosts /></Route>
-        <Route exact path="/addpost"><AddPost /></Route>
-        
-        <PrivateRoute exact path="/dashboard" component={Dashboard} />
-        <Route path="/loggedout"><LoggedOut /></Route>
-      </Switch>
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <HeaderBar />
+            <Switch>
+              <Route exact path="/"><Home /></Route>
+              <Route exact path="/register"><SignUpForm /></Route>
+              <Route exact path="/login"><LoginForm /></Route>
+              <Route exact path="/allposts"><AllPosts /></Route>
+              <Route exact path="/addpost"><AddPost /></Route>        
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <Route path="/loggedout"><LoggedOut /></Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
---
+++ expat-journal-fe/src/assets/StyledComponents.js
@@ -0,0 +1,19 @@
+// dependency imports
+import styled from "styled-components";
+
+export const Navbar = styled.div`
+width: 100%;
+background: rgba(0, 0, 0, 0.4);
+display: flex;
+flex-direction: row;
+margin: 0 auto;
+justify-content: space-around;
+padding: 1% 0;
+`
+
+export const OtherLinks = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+width: 70%;
+`
